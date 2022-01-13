50696def3a7b866054f3891a492989cd3d88ecdc
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-01-14 00:22:11 +0900

    毎日アルゴ式/集合練習問題2完了

--- algo-method/src/main/kotlin/everyDay-algoMethod/set/Question02.kt
+++ algo-method/src/main/kotlin/everyDay-algoMethod/set/Question02.kt
@@ -1,12 +1,14 @@
 package `everyDay-algoMethod`.set
 
-// 共通する数
-// https://algo-method.com/tasks/586
+// 数の合併 (A ∪ B)
+// https://algo-method.com/tasks/590
 
 fun main() {
     val(N, M) = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
     val listA = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
     val listB = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
-    val answer = listOf<Int>()
-    listA.forEach { it -> if (listB.contains(it)) println(it) }
+    val answer = mutableListOf<Int>()
+    listA.forEach { it -> answer.add(it) }
+    listB.forEach { it -> if (!listA.contains(it)) answer.add(it) }
+    answer.sorted().forEach { it -> println(it) }
 }
