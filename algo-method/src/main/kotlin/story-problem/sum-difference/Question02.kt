d8eab14c49d2024d8ddf576401bdfe420815995b
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-01-17 23:21:25 +0900

    文章題/和差算前編完了

---
+++ algo-method/src/main/kotlin/story-problem/sum-difference/Question02.kt
@@ -0,0 +1,8 @@
+package `story-problem`.`sum-difference`
+
+// 問題
+// https://algo-method.com/tasks/128
+
+fun main() {
+    println((186 + 12) / 2)
+}
---
+++ algo-method/src/main/kotlin/story-problem/sum-difference/Question05.kt
@@ -0,0 +1,8 @@
+package `story-problem`.`sum-difference`
+
+// 問題
+// https://algo-method.com/tasks/139
+
+fun main() {
+    println((1200 - 200) / 2)
+}
