9fff59aee051d6d127a36a0daae8cf2b68f289db
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-01-30 20:29:51 +0900

    等差数列/等比数列練習問題4まで完了

---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/sequence/Question03.kt
@@ -0,0 +1,10 @@
+package `everyDay-algoMethod`.sequence
+
+// 問題
+// https://algo-method.com/tasks/652
+
+fun main() {
+    val N = readLine()!!.toLong()
+    val answer = (1 + N) * N / 2
+    println(answer)
+}
