ffb1fc5c4d46db1e80e3e0aaca4a0db50236f2c0
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-02-04 22:45:02 +0900

    確率と期待値練習問題2まで完了

---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/ probability/Question01.kt
@@ -0,0 +1,10 @@
+package `everyDay-algoMethod`.` probability`
+
+// 問題
+// https://algo-method.com/tasks/759
+
+fun main() {
+    val N = readLine()!!.toInt()
+    val answer:Double  = 1.0 / N.toDouble()
+    println(answer)
+}
