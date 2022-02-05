7c7d2677353b829144c0b37f17d7b5b18b8dbe8b
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-02-05 21:18:17 +0900

    毎日アルゴ式/確率練習問題6まで完了

---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/ probability/Question05.kt
@@ -0,0 +1,24 @@
+package `everyDay-algoMethod`.` probability`
+
+// 問題
+// https://algo-method.com/tasks/768
+
+fun main() {
+    // 入力
+    val(N, M) = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
+    val list = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
+    val arrayP = IntArray(N + 1)
+    arrayP[0] = 0
+    for (index in 1..N) {
+        arrayP[index] = list[index - 1]
+    }
+    val D = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
+
+    var answer: Double = 1.0
+    // 各回の確率は独立している
+    for (index in D.indices) {
+        answer *= (arrayP[D[index]].toDouble() / 100.0)
+    }
+    // 出力
+    println(answer)
+}
---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/ probability/Question06.kt
@@ -0,0 +1,30 @@
+package `everyDay-algoMethod`.` probability`
+
+// 問題
+// https://algo-method.com/tasks/761
+
+fun main() {
+    // 入力
+    val(N, X) = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
+    val list = readLine()!!.split(" ").mapNotNull(String::toIntOrNull)
+    val arrayP = IntArray(N + 1)
+    arrayP[0] = 0
+    for (index in 1..N) {
+        arrayP[index] = list[index - 1]
+    }
+
+    // 2回の出目の和が、Xになっているかどうかチェック
+    var answer = 0.0
+    for (indexOne in 1..N) {
+        for (indexTwo in 1..N) {
+            if (indexOne + indexTwo == X) {
+                val a = arrayP[indexOne].toDouble() / 100.0
+                val b = arrayP[indexTwo].toDouble() / 100.0
+                answer += a * b
+            }
+        }
+    }
+
+    // 出力
+    println(answer)
+}
