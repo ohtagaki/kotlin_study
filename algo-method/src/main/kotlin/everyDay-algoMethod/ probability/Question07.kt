6ff5be8d9e84b68fe2d836fdadc6965d4432273d
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-02-07 23:39:10 +0900

    20220207分練習問題完了

---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/ probability/Question07.kt
@@ -0,0 +1,13 @@
+package `everyDay-algoMethod`.` probability`
+
+fun main() {
+    val N = readLine()!!.toInt()
+
+    // １回の試行で、外れくじを引く確率
+    val play = 1.0 - 1.0 / N.toDouble()
+
+    // 答えを出力
+    val answer = 1 - play * play
+
+    println(answer)
+}
