c072d993eae8ec317c4164bd724e46fb93704701
ohtagaki <sweetlife0815hiikor@gmail.com>
2022-02-09 23:20:35 +0900

    20220209分練習問題完了

---
+++ algo-method/src/main/kotlin/everyDay-algoMethod/ probability/Question08.kt
@@ -0,0 +1,18 @@
+package `everyDay-algoMethod`.` probability`
+
+// 問題
+// https://algo-method.com/tasks/765
+
+import kotlin.math.pow
+
+fun main() {
+    // 入力
+    val N = readLine()!!.toInt()
+
+    // 自分と他人Aが誕生日が異なる確率
+    val other = 1.0 - 1.0 / 365.0
+
+    // 答えを出力
+    val answer = 1 - other.pow(N)
+    println(answer)
+}
